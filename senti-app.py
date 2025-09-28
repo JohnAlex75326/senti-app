130198de0b18d1a7ff05e01d70f7c0c9811ff0dc
JohnAlex75326 <126377266+JohnAlex75326@users.noreply.github.com>
2025-09-29 06:08:29 +0800

    Add files via upload

--- senti-app.py
+++ senti-app.py
@@ -19,7 +19,7 @@ sentiment_model = pipeline("sentiment-analysis")  # Hugging Face BERT sentiment
 # Streamlit UI
 # -----------------------------
 st.title("📝 SentAL - Sentiment & Insights Analyzer")
-st.write("Input a **text** or Upload an **image (OCR)** and get **a summary, AI-powered sentiment, entities, and charts**")
+st.write("Input a **text** or Upload an **image (OCR)** and get **a summary, AI-powered sentiment analysis, entities, and charts**")
 
 # Input type
 option = st.radio("Choose Input Type:", ("Text", "Image"))
